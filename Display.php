e4c525ad040a0f22a2c6989d9d50140c02100904
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-23 16:14:33 +0100

    used query to retrieve data from the db

--- Display.php
+++ Display.php
@@ -5,3 +5,4 @@ $db = new PDO('mysql:host=db; dbname= House_Plants', 'root', 'password');
 //Told PDO to return an associative array.
 $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
 
+$query= $db -> query('SELECT `latin_name`, `level_of_watering`, `level_of_sunlight` FROM House_Plants');
