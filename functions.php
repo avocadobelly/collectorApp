a286c322a100dd86aa18ea12df8ec42c90475c47
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-24 16:29:22 +0100

    removed unnecessary concatenation

--- functions.php
+++ functions.php
@@ -10,11 +10,11 @@
 function list_of_plants($House_Plants) {
     $result = '';
     foreach($House_Plants as $plant) {
-        $result .= '<div>' .
-                '<h1>'. $plant['latin_name'] . '</h1>' .
-                '<h3>' . 'Level of Watering: ' . $plant['level_of_watering'] . '</h3>' .
-                '<h3>' . 'Level of Sunlight: ' . $plant['level_of_sunlight'] . '</h3>' .
-            '</div>';
+        $result .= '<div>
+                        <h1>'. $plant['latin_name'] . '</h1>
+                        <h3>' . 'Level of Watering: ' . $plant['level_of_watering'] . '</h3>
+                        <h3>' . 'Level of Sunlight: ' . $plant['level_of_sunlight'] . '</h3>
+                    </div>';
     }
     return $result;
 }
