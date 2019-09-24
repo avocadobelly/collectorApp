af8e75a91bc87f7a219188884d8bb0355ef45f45
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-24 13:41:51 +0100

    put foreach loop into a function

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
-foreach($House_Plants as $plant) {
-    echo $plant ['latin_name'], ['watering_level'], ['level_of_sunlight'];
+function plants_list(){
+    foreach($House_Plants as $plant) {
+        echo $plant ['latin_name'], ['watering_level'], ['level_of_sunlight'];
+    }
 }
