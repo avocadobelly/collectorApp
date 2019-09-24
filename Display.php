fb390f56ed3160d1fafba6eb6207f03ec65e63b4
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-24 12:09:44 +0100

    file wasn't recognising DB so have manually reset everything back to the end of the last task

--- Display.php
+++ Display.php
@@ -1,11 +1,11 @@
 <?php
 
 //Set up connection to the DB.
-$db = new PDO('mysql:host=db;dbname=House_Plants', 'root', 'password');
+$db = new PDO('mysql:host=db;dbname=House_Plants_2019-09-23', 'root', 'password');
 //Told PDO to return an associative array.
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
 //queried the db remotely
-$query = $db->query('SELECT `latin_name`, `level_of_watering`, `level_of_sunlight` FROM House_Plants');
+$query = $db->query('SELECT `latin_name`, `level_of_watering`, `level_of_sunlight` FROM `House_Plants`');
 //fetch all data listed in query and store in variable
 $House_Plants = $query->fetchAll();
 //output to the browser
