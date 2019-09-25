6cae7d71833e4dcc49017f68993a39af5a6d903f
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-25 17:24:38 +0100

    Added basic structure for html form

--- Input.php
+++ Input.php
@@ -1,1 +1,16 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Input Page</title>
+    </head>
+<body>
+    <form>
+
+    </form>
+</body>
+</html>
