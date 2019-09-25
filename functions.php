551d1164ea386575f80f65fa39c292aaf48bd47b
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-25 16:58:57 +0100

    deleted unnecessary comments

--- functions.php
+++ functions.php
@@ -23,9 +23,7 @@ function getDataFromDatabase(): array {
 function list_of_plants(array $House_Plant_DB): string{
     $result = '';
     foreach($House_Plant_DB as $plant) {
-        //if the result returns a string and the plant variable returns an array then the code will run
         if (gettype($plant['latin_name']) === 'string'  && gettype($plant['level_of_watering'] === 'string') && gettype($plant['level_of_sunlight'] === 'string')){
-            //if the result returns anything but a string or the plant variable returns anything but an array then an error message will be displayed
             $result .= '<div>
                         <h1>' . $plant['latin_name'] . '</h1>
                         <h3>' . 'Level of Watering: ' . $plant['level_of_watering'] . '</h3>
