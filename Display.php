590f9d5e3d57fe5204c160b29de34c29de538c67
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-25 16:16:54 +0100

    Created a function for calling the database to clean up the code. DocBlocked the new function and stored it in the fucntions file. Called the new fuction in the display file

--- Display.php
+++ Display.php
@@ -1,15 +1,8 @@
 <?php
 //sets up connection to functions file
 require_once('functions.php');
+$House_Plant_DB = getDataFromDatabase();
 
-//Set up connection to the DB.
-$db = new PDO('mysql:host=db;dbname=House_Plants_2019-09-23', 'root', 'password');
-//Told PDO to return an associative array.
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
-//queried the db remotely
-$query = $db->query('SELECT `latin_name`, `level_of_watering`, `level_of_sunlight` FROM `House_Plants`');
-//fetch all data listed in query and store in variable
-$House_Plants = $query->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +15,7 @@ $House_Plants = $query->fetchAll();
 
 <?php
 //calls list_of_plants function and echoes the result
- echo list_of_plants($House_Plants);
+ echo list_of_plants($House_Plant_DB);
 ?>
 
 </body>
--- functions.php
+++ functions.php
@@ -1,16 +1,28 @@
 <?php
+/*
+*Pulls information from House Plants database.
+*
+*@return array of information in the database.
+ */
+function getDataFromDatabase(): array {
+    $db = new PDO('mysql:host=db;dbname=House_Plants_2019-09-23', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_ASSOC);
+    $query = $db->query('SELECT `latin_name`, `level_of_watering`, `level_of_sunlight` FROM `House_Plants`');
+    $House_Plant_DB = $query->fetchAll();
 
+    return $House_Plant_DB;
+}
 /*
 *Pulls information from specified data fields in the House_Plants Database.
 *
-*@param array $House_Plants is the database where all info on the plants is stored
+*@param array $House_Plant_DB is the database where all info on the plants is stored
 *@param array $plant is a row of the database from which the function will call information on plant attributes
 *
 *@return string of attributes for each plant
  */
-function list_of_plants(array $House_Plants): string{
+function list_of_plants(array $House_Plant_DB): string{
     $result = '';
-    foreach($House_Plants as $plant) {
+    foreach($House_Plant_DB as $plant) {
         //if the result returns a string and the plant variable returns an array then the code will run
         if (gettype($plant['latin_name']) === 'string'  && gettype($plant['level_of_watering'] === 'string') && gettype($plant['level_of_sunlight'] === 'string')){
             //if the result returns anything but a string or the plant variable returns anything but an array then an error message will be displayed
