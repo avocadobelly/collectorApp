22584cff3ea80418b4a5d7219facd38ba13bf9f9
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 10:21:21 +0100

    Removed line breaks, will add CSS later. Swapped text fields for radio buttons as seemed more appropriate for purpose. Added labels to the radio buttons

--- Input.php
+++ Input.php
@@ -15,18 +15,19 @@
                 Latin name of plant:
                 <input type=text" name="latin_name">
             </div>
-            <br>
             <div>
                 Level of watering needed:
-                <input type=text" name="level_of_watering">
+                <input type="radio" name="level_of_watering" value="Light"><label for="Light">Light</label>
+                <input type="radio" name="level_of_watering" value="Moderate"><label for="Moderate">Moderate</label>
+                <input type="radio" name="level_of_watering" value="Heavy"><label for="Heavy">Heavy</label>
             </div>
-            <br>
             <div>
                 Level of sunlight needed:
-                <input type=text" name="level_of_sunlight">
-                <br>
-                <input type="submit" value="Add to collection">
+                <input type="radio" name="level_of_sunlight" value="Low"><label for="Low">Low</label>
+                <input type="radio" name="level_of_sunlight" value="Moderate"><label for="Moderate">Moderate</label>
+                <input type="radio" name="level_of_sunlight" value="Bright"><label for="Bright">Bright</label>
             </div>
+            <input type="submit" value="Add to collection">
         </fieldset>
     </form>
 </body>
