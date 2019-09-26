25c5d50bdba18f7cc85a78157ac81ee6ffb715af
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 17:01:01 +0100

    created stles and css files, added very basic styling, put some divs around form and collection

--- Index.php
+++ Index.php
@@ -7,12 +7,15 @@ $House_Plant_DB = getDataFromDatabase();
     <head>
         <meta charset="UTF-8">
         <title> Display Page</title>
+        <link href="normalize.css" rel="stylesheet" type="text/css">
+        <link href="Styles.css" rel="stylesheet" type="text/css">
     </head>
 <body>
-
-<?php
- echo list_of_plants($House_Plant_DB);
-?>
-<a href = "http://localhost:1234/collectorApp/Input.php">Add to the collection</a>
+    <div>
+        <?php
+        echo list_of_plants($House_Plant_DB);
+        ?>
+        <a href = "http://localhost:1234/collectorApp/Input.php">Add to the collection</a>
+    </div>
 </body>
 </html>
--- Input.php
+++ Input.php
@@ -11,29 +11,34 @@ if (isset($_POST['latin_name']) && isset($_POST['level_of_watering']) && isset($
     <head>
         <meta charset="UTF-8">
         <title> Input Page</title>
+        <link href="normalize.css" rel="stylesheet" type="text/css">
+        <link href="Styles.css" rel="stylesheet" type="text/css">
     </head>
 <body>
-    <form method="post">
-        <fieldset>
-            <div>
-                <label for="latin_name">Latin name of plant:</label>
-                <input type="text" name="latin_name" required>
-            </div>
-            <div>
-                <label for="level_of_watering">Level of watering needed:</label>
-                <input type="radio" name="level_of_watering" value="Light" required><label for="Light" >Light</label>
-                <input type="radio" name="level_of_watering" value="Moderate"><label for="Moderate">Moderate</label>
-                <input type="radio" name="level_of_watering" value="Heavy"><label for="Heavy">Heavy</label>
-            </div>
-            <div>
-                <label for="level_of_sunlight">Level of sunlight needed:</label>
-                <input type="radio" name="level_of_sunlight" value="Low" required><label for="Low">Low</label>
-                <input type="radio" name="level_of_sunlight" value="Moderate"><label for="Moderate">Moderate</label>
-                <input type="radio" name="level_of_sunlight" value="Bright"><label for="Bright">Bright</label>
-            </div>
-            <input type="submit" value="Add to collection">
-        </fieldset>
-    </form>
-    <a href = "http://localhost:1234/collectorApp/Index.php">See Collection</a>
+    <div>
+        <form method="post">
+            <fieldset>
+                <div>
+                    <label for="latin_name">Latin name of plant:</label>
+                    <input type="text" name="latin_name" required>
+                </div>
+                <div>
+                    <label for="level_of_watering">Level of watering needed:</label>
+                    <input type="radio" name="level_of_watering" value="Light" required><label for="Light" >Light</label>
+                    <input type="radio" name="level_of_watering" value="Moderate"><label for="Moderate">Moderate</label>
+                    <input type="radio" name="level_of_watering" value="Heavy"><label for="Heavy">Heavy</label>
+                </div>
+                <div>
+                    <label for="level_of_sunlight">Level of sunlight needed:</label>
+                    <input type="radio" name="level_of_sunlight" value="Low" required><label for="Low">Low</label>
+                    <input type="radio" name="level_of_sunlight" value="Moderate"><label for="Moderate">Moderate</label>
+                    <input type="radio" name="level_of_sunlight" value="Bright"><label for="Bright">Bright</label>
+                </div>
+                <input type="submit" value="Add to collection">
+            </fieldset>
+        </form>
+        <a href = "http://localhost:1234/collectorApp/Index.php">See Collection</a>
+    </div>
+
 </body>
 </html>
