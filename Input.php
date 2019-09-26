94f66835c8430c73ffbc4473c92778b7d4989ec9
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 09:22:49 +0100

    Added input fields to the form for the different attributes

--- Input.php
+++ Input.php
@@ -10,7 +10,12 @@
     </head>
 <body>
     <form>
-
+        Latin name of plant:
+        <input type=text" name="latin_name">
+        Level of watering needed:
+        <input type=text" name="level_of_watering">
+        Level of sunlight needed:
+        <input type=text" name="level_of_sunlight">
     </form>
 </body>
 </html>
