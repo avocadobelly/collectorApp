0cb5a9a5b50268e5f158820e8be8a9f913035e64
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 09:27:34 +0100

    put all form elements into a field set

--- Input.php
+++ Input.php
@@ -10,12 +10,14 @@
     </head>
 <body>
     <form>
-        Latin name of plant:
-        <input type=text" name="latin_name">
-        Level of watering needed:
-        <input type=text" name="level_of_watering">
-        Level of sunlight needed:
-        <input type=text" name="level_of_sunlight">
+        <fieldset>
+            Latin name of plant:
+            <input type=text" name="latin_name">
+            Level of watering needed:
+            <input type=text" name="level_of_watering">
+            Level of sunlight needed:
+            <input type=text" name="level_of_sunlight">
+        </fieldset>
     </form>
 </body>
 </html>
