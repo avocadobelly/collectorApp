aeb447169ae4034bfa8d45a1dac2f130cff16299
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 09:47:03 +0100

    Added line breaks as placeholder style

--- Input.php
+++ Input.php
@@ -11,12 +11,20 @@
 <body>
     <form>
         <fieldset>
-            Latin name of plant:
-            <input type=text" name="latin_name">
-            Level of watering needed:
-            <input type=text" name="level_of_watering">
-            Level of sunlight needed:
-            <input type=text" name="level_of_sunlight">
+            <div>
+                Latin name of plant:
+                <input type=text" name="latin_name">
+            </div>
+            <br>
+            <div>
+                Level of watering needed:
+                <input type=text" name="level_of_watering">
+            </div>
+            <br>
+            <div>
+                Level of sunlight needed:
+                <input type=text" name="level_of_sunlight">
+            </div>
         </fieldset>
     </form>
 </body>
