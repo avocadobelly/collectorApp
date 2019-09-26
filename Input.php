83adc11d61689bfad67359c6f3836dfe022c6510
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-09-26 10:24:22 +0100

    Added label tags to each input field heading

--- Input.php
+++ Input.php
@@ -12,17 +12,17 @@
     <form method="post" action="/functions.php">
         <fieldset>
             <div>
-                Latin name of plant:
+                <label for="latin_name">Latin name of plant:</label>
                 <input type=text" name="latin_name">
             </div>
             <div>
-                Level of watering needed:
+                <label for="level_of_watering">Level of watering needed:</label>
                 <input type="radio" name="level_of_watering" value="Light"><label for="Light">Light</label>
                 <input type="radio" name="level_of_watering" value="Moderate"><label for="Moderate">Moderate</label>
                 <input type="radio" name="level_of_watering" value="Heavy"><label for="Heavy">Heavy</label>
             </div>
             <div>
-                Level of sunlight needed:
+                <label for="level_of_sunlight">Level of sunlight needed:</label>
                 <input type="radio" name="level_of_sunlight" value="Low"><label for="Low">Low</label>
                 <input type="radio" name="level_of_sunlight" value="Moderate"><label for="Moderate">Moderate</label>
                 <input type="radio" name="level_of_sunlight" value="Bright"><label for="Bright">Bright</label>
